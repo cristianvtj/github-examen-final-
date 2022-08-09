f6e7cf4a4cf1e639321ca907412b336f6077876e
cristianvtj <cmurillo076@misena.co>
2022-08-09 18:04:13 -0500

    Subir 1 ejercicio a la rama dev

---
+++ src/Bucle.java
@@ -0,0 +1,26 @@
+public class Bucle {
+
+
+
+    public static void main(String[] args){
+
+        int edad= 10;
+
+        for(int i=0;i<edad;i++){
+            System.out.println(i);
+        }
+
+
+        for(int i=edad; i > 0; i--){
+            System.out.println(i);
+        }
+
+    }
+
+
+
+
+
+
+
+}
