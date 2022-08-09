1093f0a269dd4bef7eb561ef4c97de8f54f4d13d
cristianvtj <cmurillo076@misena.co>
2022-08-09 17:59:18 -0500

    Subir un proyecto de Java a la rama main

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+   Scanner cmd = new Scanner(System.in);
+
+   String claveIngresada ="";
+   String claveAlmacenada ="12345";
+   boolean claveOK =false;
+
+        //Primero muestre un mensaje solicitando la clave
+        //Reciva la clave
+        //Comparar con la clave almacenada
+        //si la clave es correcta:  Mostrar mensaje de ok
+        //Si es incorrecta nuevamente va a solicitar la clave.
+
+
+       do {
+           System.out.print("Digita tu clave: ");
+           claveIngresada = cmd.nextLine();
+           if (claveIngresada.equals(claveAlmacenada)) {
+
+               claveOK = true;
+               System.out.println("La Clave es !Correcta!");
+
+           } else {
+
+               claveOK = false;
+               System.out.println("Clave !Errada! Ingresa Clave Nuevamente");
+
+           }
+       }while (claveOK==false);
+
+        //sigue con su ejecucion
+
+        System.out.println("Bienvenido a Restaurante el dorado =)");
+//...
+
+        }
+
+    }
+
+
